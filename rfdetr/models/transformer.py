424eb350bea5a1bb583432d2ec0e078932f16d56
HasnatHridoy <125564213+HasnatHridoy@users.noreply.github.com>
2025-09-25 19:37:17 +0600

    Update transformer.py

--- rfdetr/models/transformer.py
+++ rfdetr/models/transformer.py
@@ -219,7 +219,7 @@ class Transformer(nn.Module):
             mask_flatten = torch.cat(mask_flatten, 1)   # bs, \sum{hxw}
             valid_ratios = torch.stack([self.get_valid_ratio(m) for m in masks], 1)
         lvl_pos_embed_flatten = torch.cat(lvl_pos_embed_flatten, 1) # bs, \sum{hxw}, c 
-        spatial_shapes = torch.as_tensor(spatial_shapes, dtype=torch.long, device=memory.device)
+        
         level_start_index = torch.cat((spatial_shapes.new_zeros((1, )), spatial_shapes.prod(1).cumsum(0)[:-1]))
         
         if self.two_stage:
